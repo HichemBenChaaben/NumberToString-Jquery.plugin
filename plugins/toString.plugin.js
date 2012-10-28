e7579df4884930aef4aef48fbe830bcb0f363f0e
HichemBenChaabene <hichem@dubizzle.com>
2012-10-29 01:53:26 +0400

    changed method of string conversion

--- plugins/toString.plugin.js
+++ plugins/toString.plugin.js
@@ -1,10 +1,10 @@
 // jquery plugin to convert numbers to String 
 // multilanguage support 
 // Author Hichem Ben Chaabene 
-
 // usage 
 // select a div 
 // apply goString 
+
 
 (function($) {
 // What does the numbertoString plugin do?
@@ -25,66 +25,105 @@ $.fn.numbertoString = function(options) {
 
     }); // end click function 
     	function mapping(){
+    	
     	 var mappingNumber = $(opts.dictionnary1), 
     	 	 mappingString = $(opts.dictionnary2), 
     	 	 myValue 	   = getNumber($(opts.theNumber).val()), 
 			 textprefix    = ' and ', 
 			 many 	 	   = 's', 
-			 stringOutput  = '', 
+			 stringOutput  = 'this is going to be the final output',
+			 stringLength  = 0, // by default this element is set to 0
 			 mappedvalue   = '';
 			
 			// m will hold how many numbers are in the string actually 
  			
- 			var m = getHighestMultiplier(myValue); 
+ 			var stringArray = parseString(myValue); 
 
- 		     detectNumber (m,myValue); // passing the offset and the value 
+ 			var unitsString = stringArray ; 
+ 				unitsString = unitsString.substring(0,2);
+
+ 			var hundredsString = reverseString(stringArray); 
+ 				hundredsString = hundredsString.substring(2, 3); 
+
+ 			var thousandsString = reverseString(stringArray); 
+ 				thousandsString = thousandsString.substring(3,6);
+ 			
+ 			alert('the units are '+unitsString);
+ 			alert('the hundreds are '+hundredsString) ;
+ 			alert('the thousands are '+thousandsString); 
+ 			 
+ 			 for (var i = 0 ; i < stringArray.length; i++) { 
+ 			 
+ 			 	var counter = 0 ,
+ 			 		prefix_thousands = ' thousands', 
+ 			 		prefix_hundreds = ' hundreds '; 
+ 			 		num_thousands = getThousands(parseInt(stringArray));
+ 			 		//alert('the numbers there are '+ stringArray[i]); 
+ 			 }; 
+
+ 			 // mapping the number with the table 
+ 			 // building the string output
+ 			 stringOutput =  buildText(num_thousands,prefix_thousands);
+ 			 // will return how many thousands on the string 
+ 			 // display the full string output 
+ 			 displayOutput(stringOutput);
 
     	}
+    	function reverseString (argument) {
+    		// body...
+    		return argument.split("").reverse().join("");
+    	}
+    	function buildText(number,prefix){
+    		if(number>0){
+    			number= findNumber(number,$(opts.dictionnary1),$(opts.dictionnary2));
+    			return (number + prefix ) ; // twenty thousands 
+    		}else {
+    			return NaN;
+    		}
+    	}
+    	function findNumber (argument,dictionnary1,dictionnary2) {
+    		// return the value from array 2 passed by array 
+			var  positionArray1 = $.inArray ( argument ,dictionnary1,dictionnary2);
+			var  val = dictionnary2[positionArray1];
+				 return (val);
+		
+    	}
 
+    	function displayOutput (argument) {
+    		// body... 
+    		$(opts.divMessage).text(''); 
+    		$(opts.divMessage).text(argument) ;  
+    		return ;// exit fromt the function 
+
+    	}
     	function detectNumber (offset,argument) {
     		// body...
     		var offsetNumber = offset, 
-    			original_number = argument, 
-    			prefix = '' ; 
-    		// i will start building the number from 
-    		// hundreds only  
-    		// as  
-    			var tmp_num = getUnits(original_number); 
-    			alert('the units are ' + tmp_num); 
+    			original_number = argument; 
+    			var tmp_unit = getUnits(original_number);
+
+    			alert('the units are ' + tmp_unit); 
 
     	}
     	
-    	function getHighestMultiplier (argument) {
+    	// get the length of the converted number to string 
+    	
+    	function parseString (argument) {
     		var loops = argument;
     			loops = new Number(loops); 
     			loops = loops.toString(); 
-    			return loops.length; 
+    			return loops; 
     			// now we have the range of the number 
     			// we can use a reverse loop for the offset 
     	}
-    	
-    	function mappingString (unitString) {
-			var a,
-				c = unitString,
-				val; // closing the vars ici 
-			var  a = $.inArray(c,obj.dictionnary1);
-				 val = obj.dictionnary2[a];   
-					 return (val);
-		}
-    	
-    	function getThousands(num) {
-			return num/1000; 
-		}
-		
-		function getHundreds(num) {
-			return num/100;
-		}
-		function getDecimals(num) {
-			return num/10; 
-		}
+ 	
 		function getUnits(num) {
 			return num%10; 
-		} 
+		}
+		function getThousands (argument) {
+		 	// body...
+		 	return ((argument / 1000) >0 ? argument /1000 : 0); 
+		 } 
 	    function displayerror (argument) {
 	    	var errorMessage = ' please choose a valid number ';
 	 		argument.text('').text(errorMessage);
