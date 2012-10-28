b8b16a7284b40d782ccb4877b3e4165e88595326
Hichem <hichem@dubizzle.com>
2012-10-29 01:58:19 +0400

    Update plugins/toString.plugin.js
    Added another method to detect numbers  and map them 

--- plugins/toString.plugin.js
+++ plugins/toString.plugin.js
@@ -1,10 +1,10 @@
 // jquery plugin to convert numbers to String 
 // multilanguage support 
 // Author Hichem Ben Chaabene 
-
 // usage 
 // select a div 
 // apply goString 
+
 
 (function($) {
 // What does the numbertoString plugin do?
@@ -13,10 +13,10 @@ $.fn.numbertoString = function(options) {
   var opts = $.extend(true, {}, $.fn.numbertoString.defaults, options);
 
   this.each(function() {
+
     var e = $(this);
 
     $(opts.trigger).click(function(){
-
     	$(opts.divMessage).text('plugin working fine 1!! ') ; 
     	var a= $(opts.theNumber).val();
     		a = getNumber(a); 
@@ -25,64 +25,105 @@ $.fn.numbertoString = function(options) {
 
     }); // end click function 
     	function mapping(){
-
+    	
     	 var mappingNumber = $(opts.dictionnary1), 
     	 	 mappingString = $(opts.dictionnary2), 
     	 	 myValue 	   = getNumber($(opts.theNumber).val()), 
 			 textprefix    = ' and ', 
 			 many 	 	   = 's', 
-			 stringOutput  = '', 
+			 stringOutput  = 'this is going to be the final output',
+			 stringLength  = 0, // by default this element is set to 0
 			 mappedvalue   = '';
-
+			
 			// m will hold how many numbers are in the string actually 
  			
- 			var m = getHighestMultiplier(myValue); 
+ 			var stringArray = parseString(myValue); 
 
- 		     detectNumber (m,myValue); // passing the offset and the value 
+ 			var unitsString = stringArray ; 
+ 				unitsString = unitsString.substring(0,2);
 
+ 			var hundredsString = reverseString(stringArray); 
+ 				hundredsString = hundredsString.substring(2, 3); 
+
+ 			var thousandsString = reverseString(stringArray); 
+ 				thousandsString = thousandsString.substring(3,6);
+ 			
+ 			alert('the units are '+unitsString);
+ 			alert('the hundreds are '+hundredsString) ;
+ 			alert('the thousands are '+thousandsString); 
+ 			 
+ 			 for (var i = 0 ; i < stringArray.length; i++) { 
+ 			 
+ 			 	var counter = 0 ,
+ 			 		prefix_thousands = ' thousands', 
+ 			 		prefix_hundreds = ' hundreds '; 
+ 			 		num_thousands = getThousands(parseInt(stringArray));
+ 			 		//alert('the numbers there are '+ stringArray[i]); 
+ 			 }; 
+
+ 			 // mapping the number with the table 
+ 			 // building the string output
+ 			 stringOutput =  buildText(num_thousands,prefix_thousands);
+ 			 // will return how many thousands on the string 
+ 			 // display the full string output 
+ 			 displayOutput(stringOutput);
+
+    	}
+    	function reverseString (argument) {
+    		// body...
+    		return argument.split("").reverse().join("");
+    	}
+    	function buildText(number,prefix){
+    		if(number>0){
+    			number= findNumber(number,$(opts.dictionnary1),$(opts.dictionnary2));
+    			return (number + prefix ) ; // twenty thousands 
+    		}else {
+    			return NaN;
+    		}
+    	}
+    	function findNumber (argument,dictionnary1,dictionnary2) {
+    		// return the value from array 2 passed by array 
+			var  positionArray1 = $.inArray ( argument ,dictionnary1,dictionnary2);
+			var  val = dictionnary2[positionArray1];
+				 return (val);
+		
+    	}
+
+    	function displayOutput (argument) {
+    		// body... 
+    		$(opts.divMessage).text(''); 
+    		$(opts.divMessage).text(argument) ;  
+    		return ;// exit fromt the function 
 
     	}
     	function detectNumber (offset,argument) {
     		// body...
     		var offsetNumber = offset, 
-    			original_number = argument, 
-    			prefix = '' ; 
-    		// i will start building the number from 
-    		// hundreds only  
-    		// as  
-    			var tmp_num = getUnits(original_number); 
-    			alert('the units are ' + tmp_num); 
+    			original_number = argument; 
+    			var tmp_unit = getUnits(original_number);
 
+    			alert('the units are ' + tmp_unit); 
 
     	}
-    	function getHighestMultiplier (argument) {
+    	
+    	// get the length of the converted number to string 
+    	
+    	function parseString (argument) {
     		var loops = argument;
     			loops = new Number(loops); 
     			loops = loops.toString(); 
-    			return loops.length; 
+    			return loops; 
     			// now we have the range of the number 
     			// we can use a reverse loop for the offset 
     	}
-    	function mappingString (unitString) {
-			var a,
-				c = unitString,
-				val; // closing the vars ici 
-			var  a = $.inArray(c,obj.dictionnary1);
-				 val = obj.dictionnary2[a];   
-					 return (val);
-		}
-    	function getThousands(num) {
-			return num/1000; 
-		}
-		function getHundreds(num) {
-			return num/100;
-		}
-		function getDecimals(num) {
-			return num/10; 
-		}
+ 	
 		function getUnits(num) {
 			return num%10; 
-		} 
+		}
+		function getThousands (argument) {
+		 	// body...
+		 	return ((argument / 1000) >0 ? argument /1000 : 0); 
+		 } 
 	    function displayerror (argument) {
 	    	var errorMessage = ' please choose a valid number ';
 	 		argument.text('').text(errorMessage);
@@ -166,189 +207,6 @@ $.fn.numbertoString.defaults = {
 						'thousand',
 						'million',
 						'billion'
-						]
-					, 
-};
+						]};
 
 })(jQuery);
-
-
-
-
-//
-/*
-(function($)  {
-  $.fn.goString = function(options)  {
-	var defaults =  {
-		console_message : '#pluginMessages',
-		trigger: '#getSting',
-		the_number: '#my_number',
-		dictionnary1:[
-					0,
-					1,
-					2,
-					3,
-					4,
-					5,
-					6,
-					7,
-					8,
-					9,
-					10,
-					11,
-					12,
-					13,
-					14,
-					15,
-					16,
-					17,
-					18,
-					19,
-					20,
-					30,
-					40,
-					50,
-					60,
-					70,
-					80,
-					90,
-					100,
-					1000,
-					10000,
-					100000], 
-					dictionnary2:[
-						's',
-						'one', 
-						'two',
-						'three',
-						'four',
-						'five',
-						'six',
-						'seven',
-						'eight',
-						'nine',
-						'ten',
-						'eleven',
-						'twelve',
-						'thirteen',
-						'fourteen',
-						'fifteen',
-						'sixteen',
-						'seventeen',
-						'eighteen',
-						'nineteen',
-						'twenty',
-						'thirty',
-						'fourty',
-						'fifty',
-						'sixty',
-						'seventy',
-						'eighty',
-						'ninety',
-						'hundren',
-						'thousand',
-						'million',
-						'billion'
-						]
-					, 
-					humanText:'zero',
-					messageNumber:'the number is : ',
-					displayValue:0,
-					humanTextField:'myNumber',
-					finalStringOutput:'#finalStringOutput',
-					trigguer:'#goString', 	
-	};
-	var obj = $.extend(defaults,options);
-	return  this.each(function() {
-	var e = $(this); 
-	
-	e.obj.trigger.click(function () {
-		// body...
-		e.obj.console_message.innerHTML[0] = 'plugin is working correctly';
-	}); 
-	/*	e.click(function() {
-			var a = getNumber(); 
-			var t = getThousands(a); 
-			var h = getHundreds(a);
-			var finalText = mapit(t)+mapit(h); 
-			
-			$(obj.finalStringOutput)[0].innerHTML = finalText;
-
-		});
-	
-		function getNumber() {
-			var d = $the_number.val();
-			return d; 
-		}
-		/*function mapit(myValue) {
-			var myValue = myValue, 
-				textprefix = ' and ', 
-				many = 's', 
-				stringOutput = '', 
-				mappedvalue = '';
-
-			if(myValue>99&&myValue<999) {// map thousands 
-				if(myValue = = 1) {
-					stringOutput = mappedvalue+textprefix; 
-				} 
-			}else if (myValue>9&&myValue<99) { // map hundreds
-				if(myValue = 1) {
-					stringOutput = mappedvalue+textprefix; 
-				}
-			}else {// in this case we are mapping units
-				var unitString = getUnits(myValue);// get the value and then map it  
-					stringOutput = mappingString(unitString);
-					alert('the units string output is '+stringOutput);// will display the mapped units  
-				return stringOutput; 
-			} 
-
-			return stringOutput; 
-		}
-
-		function mappingString (unitString) {
-			var a,
-				c = unitString,
-				val; // closing the vars ici 
-			var  a = $.inArray(c,obj.dictionnary1);
-				 val = obj.dictionnary2[a];  
-				 alert('the value to be returned is '+val); 
-					 return (val);
-		}
-		function getThousands(num) {
-			return num/1000; 
-		}
-		function getHundreds(num) {
-			return num/100;
-		}
-		function getDecimals(num) {
-			return num/10; 
-		}
-		function getUnits(num) {
-			return num%10; 
-		}
-		// validate function 
-		function validate(a) {
-			 var isaValidNumber = a ; 
-			 return isNaN(isaValidNumber) ? null : result;
-		}
-		function unvalidNumber() {
-			alert('the number you entered is not valid, please enter a valid number '); 
-		
-	}); 
-	}
-  }
-
-})(jQuery);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
